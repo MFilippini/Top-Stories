674dcd391b36298b8df9ca5a975fb5ac0dfe2ce3
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2018-07-10 11:24:12 -0500

    Finished setting up segues

--- Top Stories/ArticlesViewController.swift
+++ Top Stories/ArticlesViewController.swift
@@ -75,6 +75,11 @@ class ArticlesViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let url = URL(string: articles[indexPath.row]["url"]!)
+        UIApplication.shared.open(url! as URL, options: [:], completionHandler: nil)
+    }
+    
     
 }
 
